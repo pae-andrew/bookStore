60ced1bbf0e153cb6bee3a9219e7a2612d7807a6
andrew <vip.paevskiy@gmail.com>
2022-04-10 22:48:30 +0300

    Add method witch search all books by author's fullName.

--- src/main/java/ru/learnUp/lesson23/hibernate/HibernateApplication.java
+++ src/main/java/ru/learnUp/lesson23/hibernate/HibernateApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import ru.learnUp.lesson23.hibernate.dao.entity.Author;
 import ru.learnUp.lesson23.hibernate.dao.entity.Book;
 import ru.learnUp.lesson23.hibernate.dao.entity.BookStorage;
+import ru.learnUp.lesson23.hibernate.dao.repository.BookRepository;
 import ru.learnUp.lesson23.hibernate.dao.services.*;
 
 @SpringBootApplication
@@ -28,11 +29,13 @@ public class HibernateApplication {
 
 		BooksOrderService booksOrderService = context.getBean(BooksOrderService.class);
 
-		log.info("authors: {}", author.getAuthors());
-		log.info("books: {}", bookService.getBooks());
-		log.info("book storages: {}", bookStorageService.getBookStorage());
-		log.info("clients: {}", clientService.getClients());
-		log.info("book orders: {}", booksOrderService.getBooksOrders());
+//		log.info("authors: {}", author.getAuthors());
+//		log.info("books: {}", bookService.getBooks());
+//		log.info("book storages: {}", bookStorageService.getBookStorage());
+//		log.info("clients: {}", clientService.getClients());
+//		log.info("book orders: {}", booksOrderService.getBooksOrders());
+		BookRepository bookRepository = context.getBean(BookRepository.class);
+		log.info("Search result: {}", bookRepository.findByAuthor("Erich Maria Remarque"));
 	}
 
 }
--- src/main/java/ru/learnUp/lesson23/hibernate/dao/repository/BookRepository.java
+++ src/main/java/ru/learnUp/lesson23/hibernate/dao/repository/BookRepository.java
@@ -1,9 +1,18 @@
 package ru.learnUp.lesson23.hibernate.dao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.learnUp.lesson23.hibernate.dao.entity.Book;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query(value = "select * from book b " +
+            "left join author a on b.author_id = a.id " +
+            "where a.full_name = :fullName",
+            nativeQuery = true)
+    List<Book> findByAuthor(String fullName);
 }
